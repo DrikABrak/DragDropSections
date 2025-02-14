c5ffb23e056c60f580b23ae607941b3a9ab481b2
DrikABrak <ctrespeuch@gmail.com>
2025-02-14 08:44:19 +0100

    Initial Commit

---
+++ DragDropSections/DragDropSectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropSectionsApp.swift
+//  DragDropSections
+//
+//  Created by Cedric Trespeuch on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DragDropSectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
